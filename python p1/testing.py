d2eb182dc65574082738efdf04e25d1fa67b64dd
Radicaljackjr45 <99960450+Radicaljackjr45@users.noreply.github.com>
2022-07-30 14:13:51 +1200

    new testing

--- python p1/testing.py
+++ python p1/testing.py
@@ -1,15 +1,25 @@
+from os import stat
+from tracemalloc import start
 from p3 import selectionSort,array
 import numpy as np
+import time
 
 averageCase=array
 bestCase=np.sort(np.copy(array))
 worstCase=np.array(bestCase)[::-1]
 
 def test_WorstCaseSelection():
+    start= time.time()
     assert np.array_equal(selectionSort(np.copy(worstCase),len(worstCase)),bestCase)
+    print(time.time()-start)
 
 def test_BestCaseSelection():
+    start= time.time()
     assert np.array_equal(selectionSort(np.copy(bestCase),len(bestCase)),bestCase)
+    print(time.time()-start)
+
 
 def test_AverageCaseSelection():
+    start= time.time()
     assert np.array_equal(selectionSort(np.copy(averageCase),len(averageCase)),bestCase)
+    print(time.time()-start)
